0c72543bba399aee7b597efdaf1da489e363056e
dev_nst <nishal.sehan@gmail.com>
2023-10-14 23:54:51 +0530

    example implementation changed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,81 +1,30 @@
 import 'package:find_size/find_size.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  double width = 200;
-  double height = 200;
-  Size containerSize = Size.zero;
-  Offset? preLocalPosition;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Find Size'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Find Size Plugin Example'),
+        ),
+        body: Center(
+          child: FindSize(
+            onChange: (Size size) {
+              print("Button size: $size");
+            },
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Click Me'),
+            ),
+          ),
+        ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FindSize(
-                onChange: (s){
-                  setState(() {
-                    containerSize = s;
-                  });
-                },
-                child: GestureDetector(
-                  onPanUpdate: (details) {
-                    setState(() {
-                      width += details.delta.dx;
-                      height += details.delta.dy;
-                    });
-                  },
-                  child: Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1
-                      ),
-                    ),
-                    child: Center(
-                      child: Text('${containerSize.width.toInt()} x ${containerSize.height.toInt()}'),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      )
     );
   }
 }
